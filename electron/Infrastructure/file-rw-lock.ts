bd252adda98e842b878029378acc89a25b016edf
Jorex <q6286142@163.com>
2017-09-07 00:34:08 +0800

    一些小调整

--- electron/Infrastructure/file-rw-lock.ts
+++ electron/Infrastructure/file-rw-lock.ts
@@ -1,1 +1,30 @@
-export class FileRWLock {};
+
+class Lock {
+    public readers: number = 0;
+    public awaitExecuteQueue: Function[] = [];
+}
+
+export class FileRWLock {
+
+    private static _defaultInstance: FileRWLock = new FileRWLock();
+
+    private static _mapInstance: Map<string, FileRWLock> = new Map<string, FileRWLock>();
+
+    private currentLock: Lock = new Lock();
+
+    private constructor() { }
+
+    public static getCurrent(key: string = ''): FileRWLock {
+
+        if (key === '') return this._defaultInstance;
+
+        if (this._mapInstance.has(key)) return this._mapInstance.get(key);
+
+        let keyFileRWLock: FileRWLock = new FileRWLock();
+        this._mapInstance.set(key, keyFileRWLock);
+        return keyFileRWLock;
+    };
+
+};
+
+
