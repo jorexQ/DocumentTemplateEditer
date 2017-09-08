45e143ccb001686550cba9f6a9278234a14bca1a
qiujiayu <q6286142@163.com>
2017-09-08 12:29:16 +0800

    一些小东西

--- electron/Infrastructure/file-rw-lock.ts
+++ electron/Infrastructure/file-rw-lock.ts
@@ -1,30 +1,44 @@
+type QueueMehodType = () => Promise < void >;
+type ReleaseLockMethodType = () => Promise < void >;
+type CallBackMehodType = (releaseLockMethod : ReleaseLockMethodType) => Promise < void >;
 
-class Lock {
-    public readers: number = 0;
-    public awaitExecuteQueue: Function[] = [];
+export interface LockOptions < T > {
+    callBack: CallBackMehodType,
+    timeout?: number,
+    scope?: T,
+    timeoutCallBack?: () => Promise < void >;
 }
 
 export class FileRWLock {
+    private static _defaultInstance : FileRWLock = new FileRWLock();
 
-    private static _defaultInstance: FileRWLock = new FileRWLock();
+    private static _mapInstance : Map < string,
+    FileRWLock > = new Map < string,
+    FileRWLock > ();
 
-    private static _mapInstance: Map<string, FileRWLock> = new Map<string, FileRWLock>();
+    private readers : number = 0;
 
-    private currentLock: Lock = new Lock();
+    private awaitExecuteQueue : Array < QueueMehodType > = new Array < QueueMehodType > ();
 
-    private constructor() { }
+    private constructor() {}
 
-    public static getCurrent(key: string = ''): FileRWLock {
+    public static getCurrent(key : string = '') : FileRWLock {
 
-        if (key === '') return this._defaultInstance;
-
-        if (this._mapInstance.has(key)) return this._mapInstance.get(key);
-
+        if(key === '') 
+            return this._defaultInstance;
+        
+        if (this._mapInstance.has(key)) 
+            return this._mapInstance.get(key);
+        
         let keyFileRWLock: FileRWLock = new FileRWLock();
-        this._mapInstance.set(key, keyFileRWLock);
+        this
+            ._mapInstance
+            .set(key, keyFileRWLock);
         return keyFileRWLock;
     };
 
+    public read < T > (option : LockOptions < T >) {
+
+        
+    }
 };
-
-
