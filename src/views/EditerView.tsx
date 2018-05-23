590ebc43f4d16be2d892c5f4810735a448df38c8
qiujiayu <q6286142@163.com>
2018-05-23 16:19:29 +0800

    更新package相关的功能

--- src/views/EditerView.tsx
+++ src/views/EditerView.tsx
@@ -1,43 +1,44 @@
 import * as React from 'react';
-import {Layout} from 'antd';
-import {HeaderBar} from '../components/editer-layout/HeaderBar';
-import {FooterBar} from '../components/editer-layout/FooterBar';
-import {LeftBar} from '../components/editer-layout/LeftBar';
-import {RightBar} from '../components/editer-layout/RightBar';
-import {MainWorkArea} from '../components/editer-layout/MainWorkArea';
+import { Layout } from 'antd';
+import { HeaderBar } from '../components/editer-layout/HeaderBar';
+import { FooterBar } from '../components/editer-layout/FooterBar';
+import { LeftBar } from '../components/editer-layout/LeftBar';
+import { RightBar } from '../components/editer-layout/RightBar';
+import { MainWorkArea } from '../components/editer-layout/MainWorkArea';
 
-interface EditerViewProps {};
+interface EditerViewProps { };
 
 interface EditerViewState {
-    headerHeight : number,
-    footerHeight : number
+    headerHeight: number,
+    footerHeight: number
 };
 
-export class EditerView extends React.Component < EditerViewProps,
-EditerViewState > {
+export class EditerView extends React.Component<EditerViewProps, EditerViewState> {
 
-    constructor() {
-        super();
+    constructor(props: EditerViewProps) {
+        super(props);
+
         this.state = {
             headerHeight: 34,
             footerHeight: 70
         }
+
     }
 
-    public render() {
+    public render () {
         return (
             <Layout>
-                <HeaderBar height={this.state.headerHeight}/>
+                <HeaderBar height={ this.state.headerHeight } />
                 <Layout>
-                    <LeftBar/>
+                    <LeftBar />
                     <Layout>
                         <Layout>
                             <MainWorkArea
-                                headerHeight={this.state.headerHeight}
-                                footerHeight={this.state.footerHeight}/>
-                            <RightBar/>
+                                headerHeight={ this.state.headerHeight }
+                                footerHeight={ this.state.footerHeight } />
+                            <RightBar />
                         </Layout>
-                        <FooterBar height={this.state.footerHeight}/>
+                        <FooterBar height={ this.state.footerHeight } />
                     </Layout>
                 </Layout>
             </Layout>
